c7b694116ff91b0d18f1b67e78b339031887b95a
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-16 22:30:02 -0300

    [FEAT: #5] - Implementando o matriz vetor utilizando o formato COO.
    close #5

--- src/library/api.f90
+++ src/library/api.f90
@@ -12,3 +12,13 @@ subroutine pcg(a, b, x, tol, maxit, neq, fprint)
     call pcg_(a, b, x, tol, maxit, neq, fprint)
 
 end subroutine pcg
+
+subroutine matvec(a, row, col, x, y, nnz, n)
+    implicit none
+    integer, intent(in) :: nnz, n
+    integer(kind=4), dimension(nnz), intent(in) :: row, col
+    real(kind=8), dimension(nnz), intent(in) :: a
+    real(kind=8), dimension(n), intent(in) :: x
+    real(kind=8), dimension(n), intent(inout) :: y
+    call matvec_coo(a, row, col, x, y)
+end subroutine matvec

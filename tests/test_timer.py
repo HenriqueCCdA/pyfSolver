c4240aa52a17c7dad109d7a0f814f9cadb06dc3f
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-18 20:12:09 -0300

    [FEAT: #14] - Medindo o tempo de solução.
    close #14

---
+++ tests/test_timer.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+import pytest
+
+from pyfsolver.timer import Timer
+
+
+def test_exec_time():
+    with Timer("Sec A") as time:
+        sleep(1.0)
+
+    assert time.get_wall_time() == pytest.approx(1.0, rel=1e-2)

b1909c33564e400bf8a3ad5504cea68c52cb097e
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-21 03:01:56 -0300

    [CONF, REFAC: #25] - Add um formatado Fortran.
    close #25

--- src/library/blas.f90
+++ src/library/blas.f90
@@ -1,15 +1,15 @@
 subroutine matvec_coo(a, row, col, x, y, nnz, n)
-    implicit none
-    integer, intent(in) :: nnz, n
-    integer, dimension(nnz), intent(in) :: row, col
-    real(kind=8), dimension(nnz), intent(in) :: a
-    real(kind=8), dimension(n), intent(in) :: x
-    real(kind=8), dimension(n), intent(inout) :: y
-    integer :: i, irow, icol
-    y = 0.d0
-    do i = 1, nnz
-        irow = row(i) + 1
-        icol = col(i) + 1
-        y(irow) = y(irow) + a(i) * x(icol)
-    enddo
+   implicit none
+   integer, intent(in) :: nnz, n
+   integer, dimension(nnz), intent(in) :: row, col
+   real(kind=8), dimension(nnz), intent(in) :: a
+   real(kind=8), dimension(n), intent(in) :: x
+   real(kind=8), dimension(n), intent(inout) :: y
+   integer :: i, irow, icol
+   y = 0.d0
+   do i = 1, nnz
+      irow = row(i) + 1
+      icol = col(i) + 1
+      y(irow) = y(irow) + a(i)*x(icol)
+   end do
 end subroutine matvec_coo
